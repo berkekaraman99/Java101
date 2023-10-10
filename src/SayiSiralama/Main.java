86bd9e18a9b88ad33e4752aec00b2d20ef50329a
Ahmet Berke Karaman <berkekaraman99@hotmail.com>
2023-10-10 21:41:36 +0300

    first commit

---
+++ src/SayiSiralama/Main.java
@@ -0,0 +1,63 @@
+package SayiSiralama;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a, b, c;
+
+        System.out.print("1. Sayı: ");
+        a = input.nextInt();
+
+        System.out.print("2. Sayı: ");
+        b = input.nextInt();
+
+        System.out.print("3. Sayı: ");
+        c = input.nextInt();
+
+
+        //BÜYÜKTEN KÜÇÜĞE SIRALAMA
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println("a > b > c");
+            }
+            else {
+                System.out.println("a > c > b");
+            }
+        } else if((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println("b > a > c");
+            } else {
+                System.out.println("b > c > a");
+            }
+        } else if((c > a) && (c > b)) {
+            if (a > b) {
+                System.out.println("c > a > b");
+            } else {
+                System.out.println("c > b > a");
+            }
+        }
+
+        //KÜÇÜKTEN BÜYÜĞE SIRALAM
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.println("a < b < c");
+            } else {
+                System.out.println("a < c < b");
+            }
+        } else if((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println("b < a < c");
+            } else {
+                System.out.println("b < c < a");
+            }
+        } else if((c < a) && (c < b)) {
+            if (a < b) {
+                System.out.println("c < a < b");
+            } else {
+                System.out.println("c < b < a");
+            }
+        }
+    }
+}
